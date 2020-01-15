633ffcd23570322e7f4d3011be33e2aceef780a6
Eduardo da Paz <00001071150546sp@aluno.educacao.sp.gov.br>
2020-01-15 09:54:03 -0300

    Desistindo da Semana OmniStack 10

--- MyCodes/cursos_online/rocket_seat/omnistack10/back-end/src/index.js
+++ MyCodes/cursos_online/rocket_seat/omnistack10/back-end/src/index.js
@@ -1,24 +1,14 @@
 const express = require('express'); //Puxa o pacote "express"
 const mongoose = require('mongoose'); //Puxa o pacote "mongoose"
+const routes = require('./routes')
 
 mongoose.connect('mongodb+srv://eduardo:eduardo@cluster0-p1je8.mongodb.net/week10?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }) //Faz a conexão com o MongoDB
 
 const app = express(); //Define a aplicação
 
+app.use(routes);
 app.use(express.json()); //O método "use" é usado para atribuir um comportamento para app em todo o código. Poderia ser usado o método "get", porém, ele só serviria para rotas GET. Como parâmetro do método "use", usa-se o método "json" para "express", para que ele possa reconhecer requisições com o formato JSON.
 
-//MÉTODOS HTTP: get (buscar), post (criar), put(editar) e delete(duh)
-
-/* Query Params: request.query (maioria das vezes com o GET)
-   Route Params: request.route (com PUT e DELETE )
-   Body: request.body (com POST e PUT) */
-
 // Banco de dados a ser utilizado: MongoDB (não-relacional)
-
-
-app.put("/users/:id", (request, response) => {
-  console.log(request.body);
-  return response.json({ message: "Olá, Mundo!" });
-});
 
 app.listen(3333);
