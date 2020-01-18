83650fc75e1b18d540c2af988bf9907c292a80d6
Eduardo da Paz <00001071150546sp@aluno.educacao.sp.gov.br>
2020-01-18 16:12:04 -0300

    Reposionamento de diretorio e descarregamento do vocabulario

--- MyCodes/cursos_online/curso_em_video/logica_programacao_algoritmos/README.txt
+++
@@ -1,2 +0,0 @@
-Os arquivos desse diretório são do tipo ".alg", escritos no programa Visualg. 
-Visualg é um interpretador de algoritmos, e creio que esse tipo de arquivo .alg só seja lido por ele.
--- MyCodes/cursos_online/rocket_seat/starter/javascript/modulo#1/exercicio#5.js
+++
@@ -1,9 +0,0 @@
-let diego = ["Javascript", "ReactJS", "Redux"],
-    gabriel = ["VueJS", "Ruby on Rails", "Elixir"];
-
-function skills(skills1, skills2) {
-    console.log(`Diego possui habilidades em ${skills1.join(", ")}.`);
-    console.log(`Gabriel possui habilidades em ${skills2.join(", ")}.`);
-};
-
-skills(diego, gabriel);
