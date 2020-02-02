9aea5bd8d236efe25ee071743114e41fe2e7e874
Eduardo da Paz <jooeduardo8@gmail.com>
2020-02-02 07:32:48 -0300

    Estudando

--- mdn/javascript/learning_area/script.js
+++ mdn/javascript/learning_area/script.js
@@ -1,5 +1,30 @@
-let myData = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
-myArray = myData.split(',');
-myArray.push('Cardiff');
-myArray.push('Bradford', 'Brighton');
-console.log(myArray)
+const header = document.querySelector('h1');
+const selector = document.querySelector('select');
+const page = document.querySelector('html');
+
+selector.onchange = function() {
+    switch (selector.value) {
+        case 'white':
+            colorChange(selector.value, 'black');
+            break;
+        case 'black':
+            colorChange(selector.value, 'white');
+            break;
+        case 'yellow':
+            colorChange(selector.value, 'purple');
+            break;
+        case 'purple':
+            colorChange(selector.value, 'greenyellow');
+            break;
+        case 'green':
+            colorChange(selector.value, 'pink');
+            break;                                        
+    }  
+};
+
+function colorChange(bgColor, textColor) {
+    page.style.background = bgColor;
+    page.style.color = textColor;
+    document.querySelector('main').style.borderColor = textColor;
+    header.textContent = `This is ${selector.value.toUpperCase()}, MODAFOCA!`;
+};
