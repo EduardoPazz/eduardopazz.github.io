b48be8dcb75be949f695533c0fa135da49cccd7e
Eduardo da Paz <jooeduardo8@gmail.com>
2020-02-04 08:20:58 -0300

    Studying

--- mdn/javascript/learning_area/script.js
+++ mdn/javascript/learning_area/script.js
@@ -1,11 +1,30 @@
-const buttons = document.querySelectorAll('button');
-const div = document.querySelector('div');
-
-buttons[0].addEventListener('click', parentColor);
-buttons[1].addEventListener('click', parentColor);
-buttons[2].addEventListener('click', parentColor);
-buttons[3].addEventListener('click', parentColor);
-
-function parentColor(e) {
-    e.target.parentNode.style.backgroundColor = e.target.value;
+function Person(first, last, age, gender, interests) {
+    this.name = {
+       first : first,
+       last : last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function() {
+        let subject = this.gender === 'male' ? 'He' : 'She';
+        let interestsString = '';
+        for (let index in this.interests) {
+            if (interests.length === 2) {
+                interestsString = `${this.interests[0]} and ${this.interests[1]}`
+                break;
+            } else if (index === interests.length-2) {
+                interestsString += `${this.interests[interests.length-2]} and ${this.interests[interests.length-1]}`
+            } /* else {
+                interestsString += `${this.interests[index]}, `
+            } */
+        };
+         alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ' + subject + ' likes ' + interestsString + '.');
+    };
+    this.greeting = function() {
+      alert('Hi! I\'m ' + this.name.first + '.');
+    };
 }
+let person = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing', 'dancing'])
+
+person.bio()
